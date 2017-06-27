8ba06768119d41400c54076c556c2b63106f358d
Hitman142 <ghb67@sbcglobal.net>
2017-06-27 11:07:35 -0500

    Add files via upload

--- Server.py
+++ Server.py
@@ -1,6 +1,6 @@
-import socket
+import socket, winsound
 
-s = socket.socket()
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 host = socket.gethostname()
 print(host)
@@ -14,6 +14,17 @@ while True:
     c, addr = s.accept()
     print('Got connection from', addr)
     a = c.recv(50)
+    str(a, 'utf-8')
+    #a = a.strip(bytes('\'', 'utf-8'))
+    a.strip(bytes('b', 'utf-8'))
     print('Received message == ',a)
-    c.send(a)
-    c.close()
+
+    if bytes("greedy", 'utf-8') in a:
+       take all files()
+    if bytes("close", 'utf-8') in a:
+       winsound.PlaySound("roblox.wav", winsound.SND_FILENAME)
+       s.close()
+       break
+    else:
+       c.send(a)
+       c.close()
