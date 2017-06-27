353d9e8b8c493e35c7da1ff88428259a8b8d9e51
Hitman142 <ghb67@sbcglobal.net>
2017-06-27 09:07:35 -0500

    Add files via upload

--- Client.py
+++ Client.py
@@ -1,23 +1,15 @@
-import socket, sys
+import socket
+from time import sleep
+s = socket.socket()
 
+host = socket.gethostname()
 
-def connect():
-    s = socket.socket()
-    TCP_IP = '128.42.52.243'
-    host = socket.gethostname()
-    port = 22
+port = 1234
 
-    s.connect((TCP_IP, port))
-    message = input(" -> ")
-    a = (b"I Am Connecting")
-   # f = open("memes.txt", "rb")
-    s.send(a)
-    while message is not 'q':
-        s.send(message.encode())
-        data = s.recv(1024).decode()
-        print ('Received from server: ' + data)
-        message = input(" -> ")
-    s.close
-    #f.close
-    print("Done") 
-connect()
+s.connect((host, port))
+print("-->")
+i = input()
+print("Sending data")
+s.sendall(bytes(i, 'utf-8'))
+print(s.recv(1024))
+print("Done sending")
--- Server.py
+++ Server.py
@@ -1,29 +1,19 @@
+import socket
 
-import socket               
+s = socket.socket()
 
-
-s = socket.socket()         
 host = socket.gethostname()
-TCP_IP = '127.0.0.1'
-ip = socket.gethostbyname(socket.gethostname())
-port = 22              
-s.bind((TCP_IP, port))        
-print(host)
-print(ip)
 
-s.listen(1)
-c, addr = s.accept()
+port = 1234
 
-print ("Got connection from" + str(addr))
+s.bind((host, port))
+
+s.listen(5)
+
 while True:
-   s = c.recv(1024).decode()
-   if not data:
-      break
-   print("from connected user: " + str(s))
-   s = str(s).upper()
-   print("sending: " + str(s))
-   c.send(s.encode())
-
-print("Done Recieving")
-c.send("Thank you for connecting")  
-c.close()
+    c, addr = s.accept()
+    print('Got connection from', addr)
+    a = c.recv(50)
+    print('Received message == ',a)
+    c.send(a)
+    c.close()
