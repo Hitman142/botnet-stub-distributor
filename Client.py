31df71a4581ed9153ef7f2504d1b74bb9c6d9f42
Hitman142 <ghb67@sbcglobal.net>
2017-06-28 21:04:08 -0500

    Add files via upload

--- Client.py
+++ Client.py
@@ -3,21 +3,28 @@ from time import sleep
 
 
 def connect():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print(".______  ._______._______._______ .______  .____________._")
+print("    :_ _   \ : .____/: .____/: ____  |:      \ : .____/\__ _:|")
+print("    |   |   || : _/\ | : _/\ |    :  ||       || : _/\   |  :|")
+print("    | . |   ||   /  \|   /  \|   |___||   |   ||   /  \  |   |")
+print("    |. ____/ |_.: __/|_.: __/|___|    |___|   ||_.: __/  |   |")
+print("    :/         :/      :/                |___|   :/     |___|")
+print("    :                                                        ")
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    host = socket.gethostbyname(socket.gethostname())
-    o = "10.217.19.33"
-    print(host)
-    ip = "10.217.19.34"
-    port = 3467
-    port2 = 3469
+host = socket.gethostbyname(socket.gethostname())
+o = "10.217.19.33"
+print(host)
+ip = "10.217.19.34"
+port = 3467
+port2 = 3469
 
-    s.connect((o, port))
-    while 1:
-        msg = input("Command To Send: ") 
-        s.send(bytes(msg, 'utf-8'))
-        if "close" in msg:
-           winsound.PlaySound("roblox.wav", winsound.SND_FILENAME)
+s.connect((o, port))
+while 1:
+    msg = input("Command To Send: ") 
+    s.send(bytes(msg, 'utf-8'))
+    if "close" in msg:
+        winsound.PlaySound("roblox.wav", winsound.SND_FILENAME)
    
 
 while True:
